f807b233e8ad463a22c9b570e0d46de631030de2
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-04-14 03:55:54 +0400

    create active link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,36 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./../styles/Navbar.module.css";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
+  const router = useRouter();
+
+  console.log(1111111111, router.pathname);
   return (
     <div className={styles.container}>
-      <Link href="/">VHV</Link>
+      <Link href="/" className={router.pathname === "/" ? styles.active : ""}>
+        VHV
+      </Link>
       <div className={styles.mainNavBar}>
-        <Link href="/projects">Projects</Link>
-        <Link href="/experience">experience</Link>
-        <Link href="/education">education</Link>
+        <Link
+          href="/projects"
+          className={router.pathname == "/projects" ? styles.active : ""}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/experience"
+          className={router.pathname == "/experience" ? styles.active : ""}
+        >
+          experience
+        </Link>
+        <Link
+          href="/education"
+          className={router.pathname == "/education" ? styles.active : ""}
+        >
+          education
+        </Link>
       </div>
     </div>
   );
