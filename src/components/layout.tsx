85bdf6f3d6c8339d8f296e97db80fdd86963c7fb
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-04-17 06:48:59 +0400

    deploy error3 navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,11 @@
 import { Footer } from "./footer";
 import styles from "./../styles/Layout.module.css";
+import NavbarComp from "./NavbarComp";
 
 export default function Layout({ children }: any) {
   return (
     <div className={styles.container}>
+      <NavbarComp />
       <main>{children}</main>
       <Footer />
     </div>
