789f4cacf8c15f33308729169c0fa60043d72d5e
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-04-17 06:23:08 +0400

    remove some comments

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,8 @@
-import useSWR from "swr";
 import { Footer } from "./footer";
 import styles from "./../styles/Layout.module.css";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: any) {
-  //   const { data, error } = useSWR('/api/navigation', fetcher)
-
-  //   if (error) return <div>Failed to load</div>
-  //   if (!data) return <div>Loading...</div>
-
   return (
     <div className={styles.container}>
       <Navbar />
