622ec28ba9a3bfe175885f9cdfc19e5cf48e793d
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-04-17 16:23:23 +0400

    fixed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     <div className={styles.container}>
       <p>Last update 02.03.2023</p>
       <a href="https://github.com/hakobyanviktoria8/my-portfolio-app">
-        My portfolio source{" "}
+        Portfolio source{" "}
         <Image
           src="/images/githubRepo.png"
           alt="linkedin"
